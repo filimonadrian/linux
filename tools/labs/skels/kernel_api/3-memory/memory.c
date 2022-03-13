420ec33895690db39ef8d86fb6a373aa4bc63252
filimonadrian <adrian.filimon@hotmail.com>
2022-03-13 14:37:36 +0200

    Lab2: ex3: Allocated task_info struct and extracted pids.
    Solved bug with print: When printing messages, the line must end
    with "\n".
    
    Signed-off-by: filimonadrian <adrian.filimon@hotmail.com>

--- tools/labs/skels/kernel_api/3-memory/memory.c
+++ tools/labs/skels/kernel_api/3-memory/memory.c
@@ -25,19 +25,35 @@ static struct task_info *task_info_alloc(int pid)
 	struct task_info *ti;
 
 	/* TODO 1: allocated and initialize a task_info struct */
+	ti = kmalloc(sizeof(struct task_info), GFP_KERNEL);
+	if (!ti) {
+		printk(KERN_ERR "Cannot malloc task_info struct");
+		return NULL;
+	}
+
+	ti->pid = pid;
+	ti->timestamp = jiffies;
 
 	return ti;
 }
 
 static int memory_init(void)
 {
+	struct task_struct *p1, *p3, *p4;
 	/* TODO 2: call task_info_alloc for current pid */
+	p1 = get_current();
+	ti1 = task_info_alloc(p1->pid);
 
 	/* TODO 2: call task_info_alloc for parent PID */
+	ti2 = task_info_alloc(p1->parent->pid);
 
 	/* TODO 2: call task_info alloc for next process PID */
+	p3 = next_task(p1);
+	ti3 = task_info_alloc(p3->pid);
 
 	/* TODO 2: call task_info_alloc for next process of the next process */
+	p4 = next_task(next_task(p1));
+	ti4 = task_info_alloc(p4->pid);
 
 	return 0;
 }
@@ -46,8 +62,17 @@ static void memory_exit(void)
 {
 
 	/* TODO 3: print ti* field values */
+	pr_info("ti1: pid: %d timestamp: %lx\n", ti1->pid, ti1->timestamp);
+	pr_info("ti2: pid: %d timestamp: %lx\n", ti2->pid, ti2->timestamp);
+	pr_info("ti3: pid: %d timestamp: %lx\n", ti3->pid, ti3->timestamp);
+	pr_info("ti4: pid: %d timestamp: %lx\n", ti4->pid, ti4->timestamp);
+
 
 	/* TODO 4: free ti* structures */
+	kfree(ti1);
+	kfree(ti2);
+	kfree(ti3);
+	kfree(ti4);
 }
 
 module_init(memory_init);
