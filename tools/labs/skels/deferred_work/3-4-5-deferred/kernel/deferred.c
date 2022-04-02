5c37220a4b44e384ae2ca0c6f0794110e9c299fe
filimonadrian <adrian.filimon@hotmail.com>
2022-04-02 21:54:29 +0300

    Lab.5: Ex 5: Added workque.
    Added work parameter in data struct and used api for
    initialization, scheduling and flush.
    
    Signed-off-by: filimonadrian <adrian.filimon@hotmail.com>

--- tools/labs/skels/deferred_work/3-4-5-deferred/kernel/deferred.c
+++ tools/labs/skels/deferred_work/3-4-5-deferred/kernel/deferred.c
@@ -43,6 +43,8 @@ static struct my_device_data {
 	/* TODO 2: add flag */
 	int flag;
 	/* TODO 3: add work */
+	struct work_struct work;
+
 	/* TODO 4: add list for monitored processes */
 	/* TODO 4: add spinlock to protect list */
 } dev;
@@ -77,9 +79,14 @@ static struct mon_proc *get_proc(pid_t pid)
 
 
 /* TODO 3: define work handler */
+void work_handler(struct work_struct *work)
+{
+	alloc_io();
+}
 
 #define ALLOC_IO_DIRECT
 /* TODO 3: undef ALLOC_IO_DIRECT*/
+#undef ALLOC_IO_DIRECT
 
 static void timer_handler(struct timer_list *tl)
 {
@@ -92,14 +99,19 @@ static void timer_handler(struct timer_list *tl)
 	if (dev.flag == TIMER_TYPE_SET)
 		return;
 	else if (dev.flag == TIMER_TYPE_ALLOC) {
+#ifdef ALLOC_IO_DIRECT
 		alloc_io();
-	} else if (dev.flag == TIMER_TYPE_MON) {
+#else
+		/* TODO 3: schedule work */
+		schedule_work(&my_data->work);
+#endif
 
+	} else if (dev.flag == TIMER_TYPE_MON) {
+		return;
 	} else if (dev.flag == TIMER_TYPE_NONE) {
 		return;
 	}
 
-		/* TODO 3: schedule work */
 		/* TODO 4: iterate the list and check the proccess state */
 			/* TODO 4: if task is dead print info ... */
 			/* TODO 4: ... decrement task usage counter ... */
@@ -181,7 +193,7 @@ static int deferred_init(void)
 	/* TODO 2: Initialize flag. */
 	dev.flag = TIMER_TYPE_NONE;
 	/* TODO 3: Initialize work. */
-
+	INIT_WORK(&dev.work, work_handler);
 	/* TODO 4: Initialize lock and list. */
 
 	cdev_init(&dev.cdev, &my_fops);
@@ -203,7 +215,9 @@ static void deferred_exit(void)
 
 	/* TODO 1: Cleanup: make sure the timer is not running after exiting. */
 	del_timer_sync(&dev.timer);
+
 	/* TODO 3: Cleanup: make sure the work handler is not scheduled. */
+	flush_scheduled_work();
 
 	/* TODO 4: Cleanup the monitered process list */
 		/* TODO 4: ... decrement task usage counter ... */
