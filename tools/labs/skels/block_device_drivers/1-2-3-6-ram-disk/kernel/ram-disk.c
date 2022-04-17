17ecbfc67799814607c40dcbacf312440dec15c0
filimonadrian <adrian.filimon@hotmail.com>
2022-04-17 20:31:37 +0300

    Lab7: Ex.2: Added a disk associated with the driver
    Signed-off-by: filimonadrian <adrian.filimon@hotmail.com>

--- tools/labs/skels/block_device_drivers/1-2-3-6-ram-disk/kernel/ram-disk.c
+++ tools/labs/skels/block_device_drivers/1-2-3-6-ram-disk/kernel/ram-disk.c
@@ -86,12 +86,21 @@ static blk_status_t my_block_request(struct blk_mq_hw_ctx *hctx,
 	struct my_block_dev *dev = hctx->queue->queuedata;
 
 	/* TODO 2: get pointer to request */
-
+	rq = bd->rq;
 	/* TODO 2: start request processing. */
+	blk_mq_start_request(rq);
 
 	/* TODO 2: check fs request. Return if passthrough. */
+	if (blk_rq_is_passthrough(rq)) {
+		printk (KERN_NOTICE "Skip non-fs request\n");
+		blk_mq_end_request(rq, BLK_STS_IOERR);
+		goto out;
+	}
 
 	/* TODO 2: print request information */
+	pr_info("Request received: start_sector %llu, total_size: %u, data_size: %u, direction %d\n",
+		blk_rq_pos(rq),	blk_rq_cur_bytes(rq),
+		blk_rq_bytes(rq), rq_data_dir(rq));
 
 #if USE_BIO_TRANSFER == 1
 	/* TODO 6: process the request by calling my_xfer_request */
@@ -100,7 +109,7 @@ static blk_status_t my_block_request(struct blk_mq_hw_ctx *hctx,
 #endif
 
 	/* TODO 2: end request successfully */
-
+	blk_mq_end_request(rq, BLK_STS_OK);
 out:
 	return BLK_STS_OK;
 }
@@ -186,11 +195,16 @@ static int __init my_block_init(void)
 	}
 
 	/* TODO 2: create block device using create_block_device */
-
+	err = create_block_device(&g_dev);
+	if (err) {
+		printk(KERN_ERR "unable to create block device\n");
+		goto out;
+	}
 	return 0;
 
 out:
 	/* TODO 2: unregister block device in case of an error */
+	unregister_blkdev(MY_BLOCK_MAJOR, MY_BLKDEV_NAME);
 	return err;
 }
 
@@ -212,7 +226,7 @@ static void delete_block_device(struct my_block_dev *dev)
 static void __exit my_block_exit(void)
 {
 	/* TODO 2: cleanup block device using delete_block_device */
-
+	delete_block_device(&g_dev);
 	/* TODO 1: unregister block device */
 	unregister_blkdev(MY_BLOCK_MAJOR, MY_BLKDEV_NAME);
 }
